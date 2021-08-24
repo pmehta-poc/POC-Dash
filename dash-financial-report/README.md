90473c70ce9617e1478f8b88fab2ec4a94c9e18a
Prateek <prateek.mehta@nuware.com>
2021-08-24 13:34:29 +0530

    Dash-POC sample for Fincial data

---
+++ dash-financial-report/README.md
@@ -0,0 +1,39 @@
+# Dash Financial Report
+
+This is a demo of the Dash-POC
+
+## Getting Started
+
+### Running the app locally
+
+First create a virtual environment with conda or venv inside a temp folder, then activate it.
+
+```
+virtualenv venv
+
+# Windows
+venv\Scripts\activate
+# Or Linux
+source venv/bin/activate
+
+```
+
+Clone the git repo, then install the requirements with pip
+
+```
+
+git clone https://github.com/pmehta-poc/POC-Dash.git
+cd dash-financial-report
+pip install -r requirements.txt
+
+```
+
+## Run the app
+
+python app.py
+
+```
+
+## About the app
+
+This is an interactive, multi-page report which displays a variety of tables, bullet points, and Plotly interactive plots in a report format. The app incorporates custom local and external CSS to display distinct pages for PDF print.
---
+++ dash-financial-report/utils.py
@@ -0,0 +1,67 @@
+import dash_html_components as html
+import dash_core_components as dcc
+
+
+def Header(app):
+    return html.Div([get_header(app), html.Br([]), get_menu()])
+
+
+def get_header(app):
+    header = html.Div(
+        [
+            html.Div(
+                [
+                    html.A(
+                        html.Img(
+                            src=app.get_asset_url("nuware-logo.png"),
+                            className="logo",
+                        )
+                    )
+                ],
+                className="row",
+            ),
+            html.Div(
+                [
+                    html.Div(
+                        [html.H5("Investment Servies sample data")],
+                        className="seven columns main-title",
+                    )
+                   
+                ],
+                className="twelve columns",
+                style={"padding-left": "0"},
+            ),
+        ],
+        className="row",
+    )
+    return header
+
+
+def get_menu():
+    menu = html.Div(
+        [
+            dcc.Link(
+                "Overview",
+                href="/dash-financial-report/overview",
+                className="tab first",
+            ),
+            dcc.Link(
+                "Price Performance",
+                href="/dash-financial-report/price-performance",
+                className="tab",
+            ),
+        ],
+        className="row all-tabs",
+    )
+    return menu
+
+
+def make_dash_table(df):
+    """ Return a dash definition of an HTML table for a Pandas dataframe """
+    table = []
+    for index, row in df.iterrows():
+        html_row = []
+        for i in range(len(row)):
+            html_row.append(html.Td([row[i]]))
+        table.append(html.Tr(html_row))
+    return table
